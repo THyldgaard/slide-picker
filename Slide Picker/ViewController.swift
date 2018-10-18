e1d1254a10de36afd5ddeb24ac90932cf4888d1a
Tonni Hyldgaard <tonni@hufsy.com>
2018-10-18 15:24:29 +0200

    Initial Commit

---
+++ Slide Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slide Picker
+//
+//  Created by Tonni Hyldgaard on 18/10/2018.
+//  Copyright © 2018 Hyldgaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
