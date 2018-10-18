8247837e34a8318d29036700eb316eaaaa9f8e80
Tonni Hyldgaard <t.hyldgaard92@gmail.com>
2018-10-18 17:06:17 +0200

    UIColor Extension
    Updated the UIColor to have a short cut for default colours.

---
+++ Slide Picker/Utils/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  Slide Picker
+//
+//  Created by Tonni Hyldgaard on 18/10/2018.
+//  Copyright © 2018 Hyldgaard. All rights reserved.
+//
+
+import UIKit
+
+class UIColor_Extension: UIColor {
+
+}
